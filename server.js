32c5f5c38772913bd6062360bb8febae6d6ad5fc
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-09-27 21:20:21 +0200

    v2
    v2

--- server.js
+++ server.js
@@ -242,5 +242,5 @@ app.post("/gallery/:picture", upload.any(), (req, res) => {
   console.log("Data z requestu REQ.FILES", req.files);
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
