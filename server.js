50026402866917a12f38a0ec03ebf8de9c99f731
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-09-27 22:07:20 +0200

    v2

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ mongoose
 
 app.use(express.static("client/build"));
 app.use("/gallery", express.static("client/build/gallery/*"));
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+});*/
 
 app.get("/gallery", (req, res) => {
   Gallery.find({}, { name: 1, path: 1, _id: 0, image: 1 }).then(
