38e089f0df21ff925fef8cc290b8dd740087026f
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-10-06 19:30:08 +0200

    v2

--- paths.js
+++ paths.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb://heroku_fx471wj0:7scfn9i9n6d9vqh4a9hngirrv4@ds121382.mlab.com:21382/galleries"
+    "mongodb://heroku_fx471wj0:7scfn9i9n6d9vqh4a9hngirrv4@ds121382.mlab.com:21382/heroku_fx471wj0"
 };
