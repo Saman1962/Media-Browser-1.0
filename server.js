b7049c5b59d6b2033342393b22ebeeca5dcf3f4c
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-10-07 08:58:28 +0200

    v2

--- server.js
+++ server.js
@@ -39,10 +39,6 @@ mongoose
 app.use(express.static("client/build"));
 app.use("/gallery", express.static("client/build/gallery/*"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("client", "build", "index.html")); //on localhost add __dirname
-});
-
 app.get("/gallery", (req, res) => {
   Gallery.find({}, { name: 1, path: 1, _id: 0, image: 1 }).then(
     gallery => {
@@ -239,6 +235,9 @@ app.post("/gallery/:picture", upload.any(), (req, res) => {
     );
   });
   console.log("Data z requestu REQ.FILES", req.files);
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("client", "build", "index.html")); //on localhost add __dirname
 });
 
 const PORT = process.env.PORT || 5000;
