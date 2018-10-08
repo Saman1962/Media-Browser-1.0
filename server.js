c7cb7b839f1c09cff3520f7aa4740d449da110f8
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-10-08 20:45:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ mongoose
   )
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
