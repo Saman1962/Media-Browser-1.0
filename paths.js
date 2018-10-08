c33aa7619c1fee48dd6cac4a12dc7f08a9e8c26c
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-10-08 20:00:48 +0200

    Update paths.js

--- paths.js
+++ paths.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb://heroku_fx471wj0:7scfn9i9n6d9vqh4a9hngirrv4@ds155278.mlab.com:55278/heroku_fx471wj0"
+  mongoURI: "mongodb://localhost:27017/heroku_fx471wj0"
 };
+// "mongodb://heroku_fx471wj0:7scfn9i9n6d9vqh4a9hngirrv4@ds155278.mlab.com:55278/heroku_fx471wj0"
