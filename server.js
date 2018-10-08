b4eb5f7dc40620a8bea6cf438699939029f2ae40
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-10-08 20:13:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html")); //on localhost add __dirname
+});
 //app.use("/gallery", express.static("client/build/gallery/*"));
 
 app.get("/gallery", (req, res) => {
@@ -236,8 +239,5 @@ app.post("/gallery/:picture", upload.any(), (req, res) => {
   console.log("Data z requestu REQ.FILES", req.files);
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html")); //on localhost add __dirname
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
