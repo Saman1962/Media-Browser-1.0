9693f8b0b08f02e971ff289571d8cadab3ce127d
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-10-09 23:24:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const fse = require("fs-extra");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 //app.use("/", express.static(__dirname + "/"));
@@ -35,8 +35,11 @@ mongoose
   )
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
+  
 app.use(express.static(path.join(__dirname, "client/build")));
-
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 app.get("/gallery", (req, res) => {
   Gallery.find({}, { name: 1, path: 1, _id: 0, image: 1 }).then(
     gallery => {
