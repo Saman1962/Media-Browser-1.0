a465a723f39d4d955a0af91609343643ce4f25cf
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-10-09 22:48:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const fse = require("fs-extra");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/", express.static(__dirname + "/"));
+//app.use("/", express.static(__dirname + "/"));
 
 const text = bodyParser.text();
 const db = require("./paths").mongoURI;
@@ -230,6 +230,7 @@ app.post("/gallery/:picture", upload.any(), (req, res) => {
 });
 /**app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});*/ 
+});*/
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
