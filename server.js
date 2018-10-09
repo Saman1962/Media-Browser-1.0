969dc33016b4e6bf9576858b7d3a5e9d4b5f2399
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-10-09 17:52:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,6 @@ app.get("/gallery", (req, res) => {
         .send("Undefined error");
     }
   );
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 app.get("/gallery/:path", (req, res) => {
@@ -66,7 +65,6 @@ app.get("/gallery/:path", (req, res) => {
     .catch(e => {
       res.status(500).send("Nedefinovaná chyba");
     });
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 app.delete("/gallery/:category", (req, res) => {
@@ -230,9 +228,9 @@ app.post("/gallery/:picture", upload.any(), (req, res) => {
   });
   console.log("Data z requestu REQ.FILES", req.files);
 });
-/*app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});*/
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
