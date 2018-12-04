9881c4daa973e1c04345708725743c51626d27ab
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-04 16:21:48 +0100

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -25,7 +25,7 @@ const Header = props => {
               <Link
                 to="/gallery/"
                 className="d-inline mt-5"
-                onClick={props.match.history.push("/gallery")}
+                onClick={props.match.history.replace("/gallery")}
               >
                 <h2 className="header-text--small ">
                   <img className="mr-5" src={IconBack} alt="Dozadu" />
