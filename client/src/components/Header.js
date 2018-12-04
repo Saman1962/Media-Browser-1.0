41aa35ddd27bc41b643856dce3092770821e9855
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-04 23:21:17 +0100

    update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,11 +23,7 @@ const Header = props => {
           <h1 className="header-text--big">Fotogaléria</h1>
           <Router forceRefresh={true}>
             <nav className="nav">
-              <Link
-                to="/gallery"
-                className="d-inline mt-5"
-                onClick={this.forceUpdate()}
-              >
+              <Link to="/gallery" className="d-inline mt-5">
                 <h2 className="header-text--small ">
                   <img className="mr-5" src={IconBack} alt="Dozadu" />
                   {props.sliced}
