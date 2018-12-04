244db2da98a2ed7dd98dc516ecf2b4e994eaa894
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-04 19:07:42 +0100

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import IconBack from "../images/category__icon-back.svg";
+import history from "../index";
 const Header = props => {
   console.log("props", props);
   if (!props.subCategory) {
