1ab489b6d09b5c7a9eb43b85dfcdb5c9547ae452
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-09 16:06:54 +0100

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import IconBack from "../images/category__icon-back.svg";
-import { useEffect } from "react";
 
 const Header = props => {
   if (!props.subCategory) {
@@ -22,14 +21,16 @@ const Header = props => {
         <header className="header text-uppercase">
           <h1 className="header-text--big">Fotogaléria</h1>
           <Router forceRefresh={true}>
+            <Route exact path="/:gallery" component={App} />
             <nav className="nav">
-              <Link to="/gallery/" className="d-inline mt-5">
+              <Link to="/gallery" className="d-inline mt-5">
                 <h2 className="header-text--small ">
                   <img className="mr-5" src={IconBack} alt="Dozadu" />
                   {props.sliced}
                 </h2>
               </Link>
             </nav>
+            <Route />
           </Router>
           <hr className="horizontal-line" />
         </header>
