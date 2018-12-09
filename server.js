0dc99fc05fa2654755d56f4881373654ff832aa4
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-09 10:18:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(
   "/gallery",
   express.static(path.join(__dirname, "client/build/gallery/"))
 );
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 const text = bodyParser.text();
 const db = require("./paths").mongoURI;
 
@@ -229,9 +233,6 @@ app.post("/gallery/:picture", upload.any(), (req, res) => {
   });
   console.log("Data z requestu REQ.FILES", req.files);
 });
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
