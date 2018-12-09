4fa0865717ef6d8eaf2bb994f8d614c910637ad8
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-09 10:00:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -230,8 +230,8 @@ app.post("/gallery/:picture", upload.any(), (req, res) => {
   console.log("Data z requestu REQ.FILES", req.files);
 });
 app.use(express.static(path.join(__dirname, "client", "build")));
-/*app.get("*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});*/
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
