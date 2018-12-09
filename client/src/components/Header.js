0c201a521d97bbeabcd82e4bff4d9e073a631d9a
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-09 11:18:05 +0100

    update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import IconBack from "../images/category__icon-back.svg";
+import { useEffect } from "react";
 
 const Header = props => {
+  useEffect(() => {
+    this.forceRefresh();
+  });
+  console.log(props);
   if (!props.subCategory) {
     return (
       <div className="container ">
