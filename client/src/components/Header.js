78d23540602f950f1dc9c7f98a26e38511b6cb13
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-11 07:50:08 +0100

    update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 import IconBack from "../images/category__icon-back.svg";
 
 const Header = props => {
+  props.history.push("/gallery/");
   if (!props.subCategory) {
     return (
       <div className="container ">
@@ -22,11 +23,7 @@ const Header = props => {
           <h1 className="header-text--big">Fotogaléria</h1>
           <Router forceRefresh={true}>
             <nav className="nav">
-              <Link
-                to="/gallery/"
-               
-                className="d-inline mt-5"
-              >
+              <Link to="/gallery/" className="d-inline mt-5">
                 <h2 className="header-text--small ">
                   <img className="mr-5" src={IconBack} alt="Dozadu" />
                   {props.sliced}
--- client/src/index.js
+++ client/src/index.js
@@ -15,12 +15,7 @@ ReactDOM.render(
   <Router forceRefresh={true}>
     <Switch>
       <Route exact path="/:gallery/" component={App} />
-      <Route
-        exact
-        path="/:gallery/:category"
-        location={{ pathname: "/gallery/" }}
-        component={App}
-      />
+      <Route exact path="/:gallery/:category" component={App} />
       <Redirect from="*" to="/gallery/" />
     </Switch>
   </Router>,
