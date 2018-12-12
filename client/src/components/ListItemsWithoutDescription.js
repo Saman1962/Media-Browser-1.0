e8493f6c23a5372668e880d721a2bc0092167760
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-12 07:45:51 +0100

    Update ListItemsWithoutDescription.js

--- client/src/components/ListItemsWithoutDescription.js
+++ client/src/components/ListItemsWithoutDescription.js
@@ -7,66 +7,63 @@ const ListItemsWithoutDescription = props => {
   const { photoIndex, match } = props;
   console.log(props);
   return (
-    <Router>
-      <div className="container">
-        <main>
-          <div className="row no-gutters align-items-start text-center">
-            {props.data.map(item => {
-              return item.image.map((items, idx) => {
-                return (
-                  <div
-                    key={idx}
-                    className="item  item--picture col-3 text-uppercase"
-                    data-id={props.idx}
-                    onMouseOver={props.handleHover}
+    <div className="container">
+      <main>
+        <div className="row no-gutters align-items-start text-center">
+          {props.data.map(item => {
+            return item.image.map((items, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className="item  item--picture col-3 text-uppercase"
+                  data-id={props.idx}
+                  onMouseOver={props.handleHover}
+                >
+                  <Link
+                    className="d-block item__link loading"
+                    to={
+                      process.env.PUBLIC_URL + `${"/gallery/" + items.fullpath}`
+                    }
+                    onClick={e => {
+                      props.handleOpen();
+                      props.handleIdx(idx);
+                    }}
                   >
-                    <Link
-                      className="d-block item__link loading"
-                      to={
+                    <img
+                      src={
                         process.env.PUBLIC_URL +
                         `${"/gallery/" + items.fullpath}`
                       }
-                      onClick={e => {
-                        props.handleOpen();
-                        props.handleIdx(idx);
-                      }}
-                    >
-                      <img
-                        src={
-                          process.env.PUBLIC_URL +
-                          `${"/gallery/" + items.fullpath}`
-                        }
-                        className="rounded mb-0 h-100  b-lazy"
-                        alt={items.name}
-                      />
-                    </Link>
-                  </div>
-                );
-              });
-            })}
-            {props.children}
-          </div>
-        </main>
-        {props.isOpen && (
-          <Lightbox
-            match={match}
-            mainSrc={props.data[0].image[photoIndex].fullpath}
-            nextSrc={
-              props.data[0].image[(photoIndex + 1) % props.data[0].image.length]
-                .fullpath
-            }
-            prevSrc={
-              props.data[0].image[
-                (photoIndex + props.data[0].image.length - 1) %
-                  props.data[0].image.length
-              ].fullpath
-            }
-            onMovePrevRequest={props.onMovePrevRequest}
-            onMoveNextRequest={props.onMoveNextRequest}
-          />
-        )}
-      </div>
-    </Router>
+                      className="rounded mb-0 h-100  b-lazy"
+                      alt={items.name}
+                    />
+                  </Link>
+                </div>
+              );
+            });
+          })}
+          {props.children}
+        </div>
+      </main>
+      {props.isOpen && (
+        <Lightbox
+          match={match}
+          mainSrc={props.data[0].image[photoIndex].fullpath}
+          nextSrc={
+            props.data[0].image[(photoIndex + 1) % props.data[0].image.length]
+              .fullpath
+          }
+          prevSrc={
+            props.data[0].image[
+              (photoIndex + props.data[0].image.length - 1) %
+                props.data[0].image.length
+            ].fullpath
+          }
+          onMovePrevRequest={props.onMovePrevRequest}
+          onMoveNextRequest={props.onMoveNextRequest}
+        />
+      )}
+    </div>
   );
 };
 
