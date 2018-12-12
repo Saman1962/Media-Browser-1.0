f780b58133aa5d127e00e7c9aa9610165fa36878
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-12 07:00:36 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ ReactDOM.render(
   <Router forceRefresh={true}>
     <Switch>
       <Route path="/:gallery/" component={App} />
-
+      <Route path="/:gallery/:category" component={App} />
       <Redirect from="*" to="/gallery/" />
     </Switch>
   </Router>,
