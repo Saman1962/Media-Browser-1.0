4d5c5be610e5e77d2880658acbb4ce087215f7c2
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2018-12-13 07:11:26 +0100

    Update ListItemsWithoutDescription.js

--- client/src/components/ListItemsWithoutDescription.js
+++ client/src/components/ListItemsWithoutDescription.js
@@ -1,69 +1,73 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Lightbox from "./Lightbox";
 
-const ListItemsWithoutDescription = props => {
-  const { photoIndex, match } = props;
-  console.log(props);
-  return (
-    <div className="container">
-      <main>
-        <div className="row no-gutters align-items-start text-center">
-          {props.data.map(item => {
-            return item.image.map((items, idx) => {
-              return (
-                <div
-                  key={idx}
-                  className="item  item--picture col-3 text-uppercase"
-                  data-id={props.idx}
-                  onMouseOver={props.handleHover}
-                >
-                  <Link
-                    className="d-block item__link loading"
-                    to={
-                      process.env.PUBLIC_URL + `${"/gallery/" + items.fullpath}`
-                    }
-                    onClick={e => {
-                      props.handleOpen();
-                      props.handleIdx(idx);
-                    }}
+class ListItemsWithoutDescription extends Component {
+  render() {
+    const { photoIndex, match } = this.props;
+    console.log("List", this.props);
+    return (
+      <div className="container">
+        <main>
+          <div className="row no-gutters align-items-start text-center">
+            {this.props.data.map(item => {
+              return item.image.map((items, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className="item  item--picture col-3 text-uppercase"
+                    data-id={this.props.idx}
+                    onMouseOver={this.props.handleHover}
                   >
-                    <img
-                      src={
+                    <Link
+                      className="d-block item__link loading"
+                      to={
                         process.env.PUBLIC_URL +
                         `${"/gallery/" + items.fullpath}`
                       }
-                      className="rounded mb-0 h-100  b-lazy"
-                      alt={items.name}
-                    />
-                  </Link>
-                </div>
-              );
-            });
-          })}
-          {props.children}
-        </div>
-      </main>
-      {props.isOpen && (
-        <Lightbox
-          match={match}
-          mainSrc={props.data[0].image[photoIndex].fullpath}
-          nextSrc={
-            props.data[0].image[(photoIndex + 1) % props.data[0].image.length]
-              .fullpath
-          }
-          prevSrc={
-            props.data[0].image[
-              (photoIndex + props.data[0].image.length - 1) %
-                props.data[0].image.length
-            ].fullpath
-          }
-          onMovePrevRequest={props.onMovePrevRequest}
-          onMoveNextRequest={props.onMoveNextRequest}
-        />
-      )}
-    </div>
-  );
-};
+                      onClick={e => {
+                        this.props.handleOpen();
+                        this.props.handleIdx(idx);
+                      }}
+                    >
+                      <img
+                        src={
+                          process.env.PUBLIC_URL +
+                          `${"/gallery/" + items.fullpath}`
+                        }
+                        className="rounded mb-0 h-100  b-lazy"
+                        alt={items.name}
+                      />
+                    </Link>
+                  </div>
+                );
+              });
+            })}
+            {this.props.children}
+          </div>
+        </main>
+        {this.props.isOpen && (
+          <Lightbox
+            match={match}
+            mainSrc={this.props.data[0].image[photoIndex].fullpath}
+            nextSrc={
+              this.props.data[0].image[
+                (photoIndex + 1) % this.props.data[0].image.length
+              ].fullpath
+            }
+            prevSrc={
+              this.props.data[0].image[
+                (photoIndex + this.props.data[0].image.length - 1) %
+                  this.props.data[0].image.length
+              ].fullpath
+            }
+            onMovePrevRequest={this.props.onMovePrevRequest}
+            onMoveNextRequest={this.props.onMoveNextRequest}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export default ListItemsWithoutDescription;
