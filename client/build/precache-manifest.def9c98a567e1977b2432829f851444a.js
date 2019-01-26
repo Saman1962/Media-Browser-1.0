536be9af4360571ca2ae431151f4733628abc78c
Saman1962 <36858504+Saman1962@users.noreply.github.com>
2019-01-26 23:05:15 +0100

    update

---
+++ client/build/precache-manifest.def9c98a567e1977b2432829f851444a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "def6bc0a5249757f03444ef09d5b5600",
+    "url": "/static/media/add-photo-icon.def6bc0a.svg"
+  },
+  {
+    "revision": "f6cf63ac50123ed5be4b",
+    "url": "/static/css/main.9189fce4.chunk.css"
+  },
+  {
+    "revision": "a33eb8cf91fc9d117b67d304562573e3",
+    "url": "/static/media/add-category-plus-symbol.a33eb8cf.svg"
+  },
+  {
+    "revision": "b92b267eb4e8edba5b3b",
+    "url": "/static/js/1.b92b267e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "09095294ec0a53f8ff05de38d02881ca",
+    "url": "/static/media/si-glyph-picture.09095294.svg"
+  },
+  {
+    "revision": "f6cf63ac50123ed5be4b",
+    "url": "/static/js/main.f6cf63ac.chunk.js"
+  },
+  {
+    "revision": "f75a9a41d081527791d308a3eba6c1d0",
+    "url": "/static/media/prev-next_icon.f75a9a41.svg"
+  },
+  {
+    "revision": "6da81cde0d286dfad87821273ed158da",
+    "url": "/static/media/category__icon-back.6da81cde.svg"
+  },
+  {
+    "revision": "9466bef8748526d925da31f74a78c75f",
+    "url": "/static/media/close-icon.9466bef8.svg"
+  },
+  {
+    "revision": "b24d2002643a59be6361161bcd0a8027",
+    "url": "/static/media/add-icon.b24d2002.svg"
+  },
+  {
+    "revision": "b92b267eb4e8edba5b3b",
+    "url": "/static/css/1.4c0ef090.chunk.css"
+  },
+  {
+    "revision": "a0338d5d1062a8a473e515266294c022",
+    "url": "/index.html"
+  }
+];
